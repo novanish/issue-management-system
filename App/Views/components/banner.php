71d86b284e599a251bac544da0c9dfd4f9d11597
novanish <anishchhetri196@gmail.com>
2024-07-14 16:01:55 +0545

    feat: update banner component with close button functionality

--- App/Views/components/banner.php
+++ App/Views/components/banner.php
@@ -3,8 +3,8 @@
 use Core\Http\Session;
 ?>
 <div class="banner <?= Session::getFlash('flash_message')['type'] ?>">
-    <p class="message"><?= Session::getFlash('flash_message')['message'] ?></p>
-    <button class="close-button" aria-label="Close">
+    <p class="banner-message"><?= Session::getFlash('flash_message')['message'] ?></p>
+    <button class="close-button" aria-label="Close" onclick="closeAlert()">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="M18 6 6 18" />
             <path d="m6 6 12 12" />
@@ -14,7 +14,7 @@ use Core\Http\Session;
 
 <script>
     function closeAlert() {
-        const alert = document.querySelector('.alert');
+        const alert = document.querySelector('.banner');
         alert.style.transform = 'translateY(-200%)';
     }
 
