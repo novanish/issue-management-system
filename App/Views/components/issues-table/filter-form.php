21b42abf01d3af34c33e573b153f3536cc9a71ee
novanish <anishchhetri196@gmail.com>
2024-07-17 20:21:14 +0545

    add default values

--- App/Views/components/issues-table/filter-form.php
+++ App/Views/components/issues-table/filter-form.php
@@ -1,19 +1,21 @@
+<?php
+
+use Constants\IssuePriority;
+use Constants\IssueStatus;
+use Core\Http\Request;
+?>
+
 <form method="GET" class="form-grid">
     <div class="form-group full-width">
         <label for="search">Search</label>
-        <input name="search" id="search" class="input-field" placeholder="Search by issue title, description, assignee or reporter name" />
+        <input name="search" id="search" class="input-field" placeholder="Search by issue title or assignee name" value="<?= Request::getQueryParameter('search', '') ?>" />
     </div>
     <div class="form-group half-width">
         <label for="status">Status</label>
         <select id="status" name="status" class="input-field">
             <?php
-
-            use Constants\IssuePriority;
-            use Constants\IssueStatus;
-            use Core\Http\Session;
-
             foreach (['All', ...IssueStatus::getAll()] as $issueStatus) : ?>
-                <option value="<?= $issueStatus ?>" <?= Session::old('status', 'All') === $issueStatus ? 'selected' : '' ?>><?= capitalize(str_replace('_', ' ', $issueStatus)) ?></option>
+                <option value="<?= $issueStatus ?>" <?= Request::getQueryParameter('status', 'All') === $issueStatus ? 'selected' : '' ?>><?= capitalize(str_replace('_', ' ', $issueStatus)) ?></option>
                 <?php endforeach; ?>>
         </select>
     </div>
@@ -21,17 +23,17 @@
         <label for="priority">Priority</label>
         <select id="priority" class="input-field" name="priority">
             <?php foreach (['All', ...IssuePriority::getAll()] as $issuePriority) : ?>
-                <option value="<?= $issuePriority ?>" <?= Session::old('priority', 'All') === $issuePriority ? 'selected' : '' ?>><?= capitalize($issuePriority) ?></option>
+                <option value="<?= $issuePriority ?>" <?= Request::getQueryParameter('priority', 'All') === $issuePriority ? 'selected' : '' ?>><?= capitalize($issuePriority) ?></option>
             <?php endforeach; ?>
         </select>
     </div>
     <div class="form-group half-width">
         <label for="start-date">Start Date</label>
-        <input type="date" name="start-date" id="start-date" class="input-field">
+        <input type="date" name="start" id="start-date" class="input-field" value="<?= Request::getQueryParameter('start', '') ?>">
     </div>
     <div class="form-group half-width">
         <label for="end-date">End Date</label>
-        <input type="date" name="end-date" id="end-date" class="input-field">
+        <input type="date" name="end" id="end-date" class="input-field" value="<?= Request::getQueryParameter('end', '') ?>">
     </div>
     <button type="submit" class="primary-btn">Filter</button>
 </form>
